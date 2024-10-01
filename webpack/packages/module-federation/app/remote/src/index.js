27c70624591ef6a9c495a5bbb7e03d45452b0566
Sunny-117 <zhiqiangfu6@gmail.com>
2024-10-01 15:40:20 +0800

    mf

--- webpack/packages/module-federation/app/remote/src/index.js
+++ webpack/packages/module-federation/app/remote/src/index.js
@@ -1,4 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(<App/>,document.getElementById('root'));
+import('./bootstrap')
+// 不能import './bootstrap'
